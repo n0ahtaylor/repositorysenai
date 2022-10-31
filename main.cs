50f5d94ba1df256ae570864a2f5850f6fe16ee06
6leonardo16 <>
2022-10-31 11:52:43 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Testendereco
+{
+     class Endereco
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Insira o seu Nome:");
+          Console.ReadLine();
+          Console.WriteLine("Insira seu CPf:");
+          Console.ReadLine();
+          Console.WriteLine("RG:");
+          Console.ReadLine();
+          Console.WriteLine("Aguarde...");
+          Console.WriteLine("Logradouro:");
+          Console.ReadLine();
+          Console.WriteLine("Número:");
+          Console.ReadLine();
+          Console.WriteLine("Tipo do Logradouro:");
+          Console.ReadLine();
+          Console.WriteLine("Você deseja tentar novamente : sim ou não?");
+          Console.ReadLine();  
+         
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
